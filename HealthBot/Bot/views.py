a8a629a15429c776904c0ee58047640415e691d1
ClownMonster <mk6386223@gmail.com>
2020-12-20 07:55:52 +0530

    login registraion done with error handling

--- HealthBot/Bot/views.py
+++ HealthBot/Bot/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse,redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterationForm
+from django.contrib import messages
 
 # rendering the landing screen
 
@@ -9,27 +10,43 @@ def landingScreen(request):
     return render(request,template_name = 'landingscreen.html')
 
 def loginScreen(request):
-    return render(request,template_name = 'login.html')
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        passwd = request.POST['password']
+        user = authenticate(request, username =  user_name, password = passwd)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('chatroom')
+        else:
+            error = "Your account not found Pls Sign up or Pls enter valid credentials" 
+            return render(request, 'login.html', {'error': error } )
+    else:
+        return render(request,'login.html',{'error':""} )
+
+    return render(request,'login.html',{'error':""} )
+
 
 def registerScreen(request):
     if request.method == 'POST':
         register = RegisterationForm(request.POST)
-        print(register.errors)
         if register.is_valid():
-            user = register.save()
+            user = register.save(commit=False)
+            user.set_password(register.cleaned_data['password'])
+            user.save()
             login(request,user)
-            
             return redirect('chatroom')
         else:
             userform = RegisterationForm()
-            render(request,'register.html',{'userform': userform } )
+            error = register.errors
+            return render(request,'register.html',{'userform': userform, 'error': error } )
     
     else:
         userform = RegisterationForm()
-        return render(request,'register.html',{'userform': userform } )
+        return render(request,'register.html',{'userform': userform,'error': "" } )
 
     userform = RegisterationForm()
-    return render(request,'register.html',{'userform': userform } )
+    return render(request,'register.html',{'userform': userform ,'error': ""} )
     
 
 @login_required
